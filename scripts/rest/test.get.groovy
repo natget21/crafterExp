5d5eba16cd6fdf83522846b4bf38cc5a0cee3f2b
admin admin <evaladmin@example.com>
2024-11-21 05:40:14 +0000

    "User admin wrote content /scripts/rest/test.get.groovy"

--- scripts/rest/test.get.groovy
+++ scripts/rest/test.get.groovy
@@ -1,21 +1,19 @@
-// def result = searchService.search {
-//     query = "/site/components/vendor"
+def result = search ("/site/components/vendor")
+return result
+
+// def topNavItems = [:]
+// def siteDir = siteItemService.getSiteTree("/site/website", 2)
+
+// if (siteDir) {
+//     def dirs = siteDir.childItems
+//     dirs.each { dir ->
+//         def dirName = dir.getStoreName()
+//         def dirItem = siteItemService.getSiteItem("/site/website/${dirName}/index.xml")
+//         if (dirItem != null) {
+//             def dirDisplayName = dirItem.queryValue('internal-name')
+//             topNavItems.put(dirName, dirDisplayName)
+//         }
+//     }
 // }
-// return result
 
-def topNavItems = [:]
-def siteDir = siteItemService.getSiteTree("/site/website", 2)
-
-if (siteDir) {
-    def dirs = siteDir.childItems
-    dirs.each { dir ->
-        def dirName = dir.getStoreName()
-        def dirItem = siteItemService.getSiteItem("/site/website/${dirName}/index.xml")
-        if (dirItem != null) {
-            def dirDisplayName = dirItem.queryValue('internal-name')
-            topNavItems.put(dirName, dirDisplayName)
-        }
-    }
-}
-
-return topNavItems
+// return topNavItems
