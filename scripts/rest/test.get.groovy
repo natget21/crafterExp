3a33609c769dabb72e602f4fa7ccbbec97a898ed
admin admin <evaladmin@example.com>
2024-11-21 05:37:07 +0000

    "User admin wrote content /scripts/rest/test.get.groovy"

--- scripts/rest/test.get.groovy
+++ scripts/rest/test.get.groovy
@@ -1,4 +1,21 @@
-def result = searchService.search {
-    query = "/site/components/vendor"
+// def result = searchService.search {
+//     query = "/site/components/vendor"
+// }
+// return result
+
+def topNavItems = [:]
+def siteDir = siteItemService.getSiteTree("/site/website", 2)
+
+if (siteDir) {
+    def dirs = siteDir.childItems
+    dirs.each { dir ->
+        def dirName = dir.getStoreName()
+        def dirItem = siteItemService.getSiteItem("/site/website/${dirName}/index.xml")
+        if (dirItem != null) {
+            def dirDisplayName = dirItem.queryValue('internal-name')
+            topNavItems.put(dirName, dirDisplayName)
+        }
+    }
 }
-return result
+
+return topNavItems
